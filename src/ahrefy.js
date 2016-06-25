9fed3759ad7c5891e726ece5fe1668afaf69c1d2
hiogawa <hi.ogawa.zz@gmail.com>
2016-06-26 00:40:36 +0900

    make multiple urls in one paragraph work

--- src/ahrefy.js
+++ src/ahrefy.js
@@ -1,4 +1,4 @@
-const URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+const URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
 
 function convertToLink(dom) {
   const parent = dom.parentNode;
